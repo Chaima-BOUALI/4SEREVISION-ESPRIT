8b4e0940fcb0c0bb581a8ec99d6480aec024a065
ChaymaBoualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-01-07 19:28:59 +0100

    EX2 : Manipulation des tableaux Associatifs / Filtres /Structures Conditionnelles

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -42,4 +42,27 @@ class ClubController extends AbstractController
 return $this->render("club/detail.html.twig",
 ['n'=> $nom, 'c'=>$club]);
     }
+
+    /**
+     * @return Response
+     * @Route ("/liste", name="liste")
+     */
+    public function Liste(){
+        $formations = array (
+            array('ref' => 'form1', 'Titre' => 'Formation Symfony 4', 'Description' => 'Formation Théorique et Pratique'
+            , 'Date_Debut' => '20/10/2020', 'Date_fin' => '21/12/2020', 'Nombre_Participants' => '10'),
+             array('ref' => 'form2', 'Titre' => 'Formation SOA ', 'Description' => 'Formation Théorique '
+             , 'Date_Debut' => '01/01/2021', 'Date_fin' => '01/03/2021', 'Nombre_Participants' => '0'),
+            array('ref' => 'form3', 'Titre' => 'Formation SpringBoot', 'Description' => 'Formation  Pratique'
+            , 'Date_Debut' => '20/10/2022', 'Date_fin' => '21/12/2022', 'Nombre_Participants' => '20')
+        );
+        return $this->render("club/liste.html.twig", ['formations' => $formations]);
+    }
+
+    /**
+     * @Route("/detail/{id}", name="detail")
+     */
+     public function Details($id){
+        return $this->render("club/detail.html.twig" , ['ref'=>$id]);
+     }
 }
