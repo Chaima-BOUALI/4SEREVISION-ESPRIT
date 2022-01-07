3e99a50825291d2b257668c50f1ff1ac458e589d
ChaymaBoualy <92319773+ChaymaBoualy@users.noreply.github.com>
2022-01-07 19:58:17 +0100

    Creation des entités / Création de la BD

---
+++ src/Entity/Club.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClubRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClubRepository::class)
+ */
+class Club
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $ref;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $DateCreation;
+
+    public function getref(): ?int
+    {
+        return $this->ref;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->DateCreation;
+    }
+
+    public function setDateCreation(?\DateTimeInterface $DateCreation): self
+    {
+        $this->DateCreation = $DateCreation;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtudiantRepository::class)
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $NCE;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Email;
+
+    public function getNCE(): ?int
+    {
+        return $this->NCE;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(?string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+}
